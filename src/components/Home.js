6eea1de4625bf54779a50177c693fdbd4bc3dd3d
imran814210 <imran814210@gmail.com>
2024-03-01 07:31:30 +0600

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Home extends Component{
+    render(){
+        return(
+            <>
+            <h1>Hello !</h1><h1>I`am Md. Imran</h1>
+            </>
+        );
+    }
+}
+export default Home;
